5af0823aed3cabd0f10729a9e9f508652da77d34
Paul Horikx <p.horikx@gmail.com>
2025-09-01 22:56:15 +0200

    fished out small unnecessary part

--- lib/plots_with_phoenix/data_generation/parquet_exporter.ex
+++ lib/plots_with_phoenix/data_generation/parquet_exporter.ex
@@ -1,7 +1,7 @@
 defmodule PlotsWithPhoenix.ParquetExporter do
   require Logger
 
-  def export_to_parquet(data_stream, column_order, output_path) do
+  def export_to_parquet(data_stream, _column_order, output_path) do
     data_list = Enum.to_list(data_stream) |> List.flatten()
     df = Explorer.DataFrame.new(data_list)
     Explorer.DataFrame.to_parquet(df, output_path)
