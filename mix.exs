1eb638e9fd1aecf85faa84e5be494cf2699e16f4
Paul Horikx <p.horikx@gmail.com>
2025-09-01 19:22:22 +0200

    fixed parquet exporter by removing the overcomplicated usage of R code

--- mix.exs
+++ mix.exs
@@ -43,6 +43,7 @@ defmodule PlotsWithPhoenix.MixProject do
       {:phoenix, "~> 1.8.0"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.13"},
+      {:explorer, "~> 0.11.1"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
