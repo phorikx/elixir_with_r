a01ce6e02b17b4fd4045b802b40d4b6638b963fc
Paul Horikx <p.horikx@gmail.com>
2025-11-25 22:15:50 +0100

    added yaml package for support in mix task

--- mix.exs
+++ mix.exs
@@ -67,7 +67,8 @@ defmodule PlotsWithPhoenix.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
-      {:poolboy, "~> 1.5"}
+      {:poolboy, "~> 1.5"},
+      {:yaml_elixir, "~> 2.9"}
     ]
   end
 
